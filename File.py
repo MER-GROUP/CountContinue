8b2cbdce97b075c977b1727f39b16b1b6ec33646
Red Alert aka Max Ramanenka <mer-group@yandex.ru>
2022-02-20 19:47:04 +0300

    CountContinue_v1.4

--- File.py
+++ File.py
@@ -3,6 +3,10 @@
 import shutil
 # модуль для работы с вводом и выводом в консоль
 import sys
+# импортируем молуль os.path
+# dirname - определяем текущую директорию
+# join - объеденяем директорию + файл (правильный путь файла)
+from os.path import dirname, join
 ###############################################################################################################
 # класс для работы с файлом
 class File:
@@ -25,12 +29,18 @@ class File:
         #print(str(arr, 'utf-8'))
         return str(str_byte, 'utf-8').split('\n')
 
-    # запись информации в файл
+    # запись информации (списка - list) в файл
     def file_write(self, out: str, arr: list) -> None:
         for i in range(len(arr)):
             arr[i] += '\n'
         with open(out, 'w') as f:
             f.writelines(arr)
+
+    # запись информации (словаря - dict) в файл
+    def file_write_dict(self, out: str, dictor: dict) -> None:
+        with open(out, 'w') as f:
+            for k,v in dictor.items():
+                f.write(f'{k} {v}\n')
 
     # вывод в консоль содержимого файла (обычный текстовый файл)
     def file_output_console(self, arr: list) -> None:
@@ -43,25 +53,35 @@ class File:
     def file_output_console_utf8(self, file: str) -> None:
         with open(file, 'r', encoding='utf-8') as f:
             shutil.copyfileobj(f, sys.stdout)
-
+###############################################################################################################
 # проверка класса и методов
 # если программа не модуль то выполнить условие
 if __name__ == '__main__':
     def main():
-        FILE = 'CountContinue/test.py'
-        #FILE = 'CountContinue/test.txt'
-        OUT = 'CountContinue/out.txt'
+        # определяем текущую директорию, гбе будет храниться файл
+        CURRENT_DIR = dirname(__file__)
+        # задаем имя файла для чтения данных
+        FILENAME = './FilesForSearch/test_read.txt'
+        # объединяем текущую директорию и находящейся в ней файл
+        FILE_PATH = join(CURRENT_DIR, FILENAME)
+        # задаем имя файла для вывода результатов
+        OUT_FILE = 'FilesForSearch/test_write.txt'
         f = File()
-        #arr = f.file_read(FILE)
-        #f.file_output_console(arr)
-        #f.file_output_console_utf8(FILE)
+        #arr = f.file_read(FILE_PATH)
+        #f.file_output_console(FILE_PATH)
+        #f.file_output_console_utf8(FILE_PATH)
         ############################
-        arr = f.file_read_utf8(FILE)
+        # считываем файл с данными
+        arr = f.file_read_utf8(FILE_PATH)
         #f.file_output_console(arr)
         ############################
+        # вывести первую строку в консоль
         print(arr[0])
+        # подсчет количества строк
         print(len(arr))
         ############################
-        f.file_write(OUT, arr)
+        # записываем результат в файл
+        f.file_write(OUT_FILE, arr)
 
     main()
+###############################################################################################################
